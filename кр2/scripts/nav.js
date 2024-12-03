43c23a38e5545a5ea1571d948a67301574db2479
eudalova <uddalovalizza@gmail.com>
2024-12-04 00:46:18 +0300

    Delete кр2 directory

--- кр2/scripts/nav.js
+++
@@ -1,8 +0,0 @@
-
-const currentLocation = window.location.pathname.split("/").pop();
-const navLinks = document.querySelectorAll('.nav-link');
-  navLinks.forEach(link => {
-    if (link.getAttribute('href') === currentLocation) {
-      link.classList.add('active');
-    }
-})
